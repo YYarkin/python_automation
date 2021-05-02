41e746ad2c438f3e4991f1b59ad3c9c17e5b4a48
YYarkin <Yarkin73@gmail.com>
2021-05-02 19:53:46 +0300

    Ex5

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,10 +12,10 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
-    app.create_contact(
+    app.session.login(username="admin", password="secret")
+    app.contact.create_contact(
         Contact(firstname="Autotest", middlename="Autotest", lastname="Autotestov", nickname="AT", title="Python",
                 company="BOBRY", address="River", home_phone="8", mobile_phone="800", work_phone="535", fax="3535",
                 email="BobryVsegdaDobry", homepage="google.com", bday="1", bmonth="January", byear="1991", aday="1",
                 amonth="January", ayear="1991", address2="River street", phone2="house"))
-    app.logout()
+    app.session.logout()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,12 +12,12 @@ def app(request):
 
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
-    app.create_group(Group(name="rerererere", header="rerere", footer="rerererere"))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.group.create(Group(name="rerererere", header="rerere", footer="rerererere"))
+    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.group.create(Group(name="", header="", footer=""))
+    app.session.logout()
