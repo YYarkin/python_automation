88b1ec93a0ede4a4bce73b3a7bba8a16097bd370
YYarkin <Yarkin73@gmail.com>
2021-05-15 15:19:41 +0300

    Ex13

--- fixture/contact.py
+++ fixture/contact.py
@@ -39,9 +39,10 @@ class ContactHelper:
         self.return_to_home_page()
         self.contact_cache = None
 
-    def delete_first_contact(self):
+    def delete_contact_by_index(self, index):
         wd = self.app.wd
-        self.select_first_contact()
+        self.open_home_page()
+        self.select_contact_by_index(index)
         # submit deletion
         wd.find_element_by_xpath("//input[@value= 'Delete']").click()
         wd.switch_to_alert().accept()
@@ -49,19 +50,30 @@ class ContactHelper:
         self.open_home_page()
         self.contact_cache = None
 
+    def delete_first_contact(self):
+        self.delete_contact_by_index(0)
+
+    def select_contact_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
+
     def select_first_contact(self):
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
 
-    def modify_first_contact(self, contact):
+    def modify_contact_by_index(self, index, contact):
         wd = self.app.wd
+        self.open_home_page()
         # select first contact and init contact edit
-        wd.find_element_by_xpath("//tr[2]/td[8]/a").click()
+        wd.find_element_by_xpath("//tr[%s]/td[8]/a" % (index + 2)).click()
         self.fill_contact_form(contact)
         # submit contact edit
         wd.find_element_by_name("update").click()
         self.return_to_home_page()
         self.contact_cache = None
+
+    def modify_first_contact(self, contact):
+        self.modify_contact_by_index(0, contact)
 
     def fill_field(self, field_name, text):
         wd = self.app.wd
@@ -76,7 +88,6 @@ class ContactHelper:
             Select(wd.find_element_by_name(field_name)).select_by_visible_text(text)
 
     def fill_contact_form(self, contact):
-        wd = self.app.wd
         self.fill_field("firstname", contact.firstname)
         self.fill_field("middlename", contact.middlename)
         self.fill_field("lastname", contact.lastname)
--- fixture/group.py
+++ fixture/group.py
@@ -41,24 +41,31 @@ class GroupHelper:
         self.open_home_page()
         self.group_cache = None
 
-    def delete_first_group(self):
+    def delete_group_by_index(self, index):
         wd = self.app.wd
         self.open_group_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_group_page()
         self.open_home_page()
         self.group_cache = None
 
+    def delete_first_group(self):
+        self.delete_group_by_index(0)
+
+    def select_group_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
+
     def select_first_group(self):
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
 
-    def modify_first_group(self, new_group_data):
+    def modify_group_by_index(self, index, new_group_data):
         wd = self.app.wd
         self.open_group_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # open modification form
         wd.find_element_by_name("edit").click()
         self.fill_group_form(new_group_data)
@@ -67,6 +74,9 @@ class GroupHelper:
         self.return_to_group_page()
         self.open_home_page()
         self.group_cache = None
+
+    def modify_first_group(self, new_group_data):
+        self.modify_group_by_index(0, new_group_data)
 
     def fill_group_form(self, group):
         self.fill_field("group_name", group.name)
