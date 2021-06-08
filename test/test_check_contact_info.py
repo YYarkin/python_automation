e1252fda914ce5ef2eb1ffe36d041ae3e58d0d5e
YYarkin <Yarkin73@gmail.com>
2021-06-08 20:58:43 +0300

    Ex21up

--- test/test_check_contact_info.py
+++ test/test_check_contact_info.py
@@ -1,4 +1,5 @@
 import re
+from functools import reduce
 from random import randrange
 
 
@@ -7,9 +8,14 @@ def test_compare_contact_info_on_home_page_with_db(app, db):
     db_contacts = db.get_contact_list()
     assert len(home_page_contacts) == len(db_contacts)
     for contact_from_home_page in home_page_contacts:
-        contact_from_db = db_contacts[db_contacts.index(contact_from_home_page)]
+        # for contact_from_db in db_contacts:
+        #   if contact_from_db.id == contact_from_home_page.id:
+        #       break
+        contact_from_db = reduce(lambda x: x[0], filter(lambda x: x.id == contact_from_home_page.id, db_contacts))
+        assert contact_from_home_page.firstname == merge_like_on_home_page(contact_from_db.firstname)
+        assert contact_from_home_page.lastname == merge_like_on_home_page(contact_from_db.lastname)
+        assert contact_from_home_page.address == merge_like_on_home_page(contact_from_db.address)
 
-        assert contact_from_home_page.address == contact_from_db.address
         assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_db)
         assert contact_from_home_page.all_email_from_home_page == merge_email_like_on_home_page(contact_from_db)
 
@@ -19,9 +25,9 @@ def test_check_contact_info_on_home_page(app):
     index = randrange(len(home_page_contacts))
     contact_from_home_page = home_page_contacts[index]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-    assert contact_from_home_page.address == contact_from_edit_page.address
+    assert contact_from_home_page.firstname == merge_like_on_home_page(contact_from_edit_page.firstname)
+    assert contact_from_home_page.lastname == merge_like_on_home_page(contact_from_edit_page.lastname)
+    assert contact_from_home_page.address == merge_like_on_home_page(contact_from_edit_page.address)
 
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.all_email_from_home_page == merge_email_like_on_home_page(contact_from_edit_page)
@@ -39,3 +45,7 @@ def merge_phones_like_on_home_page(contact):
 def merge_email_like_on_home_page(contact):
     return re.sub(" +", " ", "\n".join(filter(lambda x: x != "", filter(lambda x: x is not None, [
         contact.email1.strip(), contact.email2.strip(), contact.email3.strip()]))))
+
+
+def merge_like_on_home_page(value):
+    return re.sub(" +", " ", value.strip())
